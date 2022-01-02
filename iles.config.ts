d085706253284389985627a91bff2aef4c5c251f
Titouan Mathis <titouan@studiometa.fr>
2022-01-02 14:42:22 +0100

    Fix ESM require error

--- iles.config.ts
+++ iles.config.ts
@@ -1,3 +1,21 @@
-import { defineConfig } from 'iles'
+import { promises as fs } from 'fs';
+import { join } from 'path';
+import { defineConfig, resolveConfig } from 'iles';
 
-export default defineConfig({})
+export default defineConfig({
+	vite: {
+		plugins: [
+			// Comment/uncomment the following lines to toggle the "Error [ERR_REQUIRE_ESM]: Must use import to load ES Module" error
+			{
+				name: 'fix-ssr-require-esm-error',
+				async writeBundle() {
+					const config = await resolveConfig();
+					fs.writeFile(
+						join(config.tempDir, 'package.json'),
+						JSON.stringify({ type: 'commonjs' })
+					);
+				},
+			},
+		],
+	},
+});
