8526cff4f56c787e957fab53035429f0b43600e0
Titouan Mathis <titouan@studiometa.fr>
2022-01-03 10:43:12 +0100

    Use remark-gfm with type CJS

--- iles.config.ts
+++ iles.config.ts
@@ -1,21 +1,25 @@
 import { promises as fs } from 'fs';
 import { join } from 'path';
 import { defineConfig, resolveConfig } from 'iles';
+import remarkGfm from 'remark-gfm';
 
 export default defineConfig({
-	vite: {
-		plugins: [
-			// Comment/uncomment the following lines to toggle the "Error [ERR_REQUIRE_ESM]: Must use import to load ES Module" error
-			{
-				name: 'fix-ssr-require-esm-error',
-				async writeBundle() {
-					const config = await resolveConfig();
-					fs.writeFile(
-						join(config.tempDir, 'package.json'),
-						JSON.stringify({ type: 'commonjs' })
-					);
-				},
-			},
-		],
+	markdown: {
+		remarkPlugins: [remarkGfm],
 	},
+	// vite: {
+	// 	plugins: [
+	// 		// Comment/uncomment the following lines to toggle the "Error [ERR_REQUIRE_ESM]: Must use import to load ES Module" error
+	// 		{
+	// 			name: 'fix-ssr-require-esm-error',
+	// 			async writeBundle() {
+	// 				const config = await resolveConfig();
+	// 				fs.writeFile(
+	// 					join(config.tempDir, 'package.json'),
+	// 					JSON.stringify({ type: 'commonjs' })
+	// 				);
+	// 			},
+	// 		},
+	// 	],
+	// },
 });
